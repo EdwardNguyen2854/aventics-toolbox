dbb26f293ea493ab240f59315b36d04515887d77
Nguyễn Khánh Duy <88141980+EdwardNguyen2854@users.noreply.github.com>
2026-09-08 14:45:25 +0700

    chore: set application version to 0.5.0

--- include/app/Version.h
+++ include/app/Version.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define AVT_VERSION_MAJOR 0
-#define AVT_VERSION_MINOR 4
-#define AVT_VERSION_PATCH 1
-#define AVT_VERSION_STRING "0.4.1"
-#define AVT_VERSION_WSTRING L"0.4.1"
+#define AVT_VERSION_MINOR 5
+#define AVT_VERSION_PATCH 0
+#define AVT_VERSION_STRING "0.5.0"
+#define AVT_VERSION_WSTRING L"0.5.0"
 #define AVT_PRODUCT_NAME_W L"Aventics Toolbox"
