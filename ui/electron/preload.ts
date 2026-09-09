6cf11aea1fe105c340a7022a434d5e9f10324c51
Nguyễn Khánh Duy <88141980+EdwardNguyen2854@users.noreply.github.com>
2026-09-09 11:34:14 +0700

    Expose Similar CAD UI bridge

--- ui/electron/preload.ts
+++ ui/electron/preload.ts
@@ -9,3 +9,22 @@ contextBridge.exposeInMainWorld("aventicsBridge", {
     ipcRenderer.on("aventics:state", (_event, data) => listener({ data }));
   }
 });
+
+contextBridge.exposeInMainWorld("aventicsSimilarCad", {
+  postMessage(message: string) {
+    ipcRenderer.send("aventics:similar-send", String(message));
+  },
+  addEventListener(type: string, listener: (event: { data: unknown }) => void) {
+    if (type !== "message" || typeof listener !== "function") return;
+    ipcRenderer.on("aventics:similar-state", (_event, data) => listener({ data }));
+  },
+  chooseImage() {
+    return ipcRenderer.invoke("aventics:similar-choose-image");
+  },
+  chooseFolder(defaultPath = "") {
+    return ipcRenderer.invoke("aventics:similar-choose-folder", String(defaultPath || ""));
+  },
+  locate(filePath: string) {
+    return ipcRenderer.invoke("aventics:similar-locate", String(filePath || ""));
+  }
+});
